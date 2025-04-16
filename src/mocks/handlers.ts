df3b3ddff1f6972f48fa2f46830aafa301da9543
wataru <wataru.oguchi.0to1@gmail.com>
2025-04-16 15:01:52 -0700

    [#example] How the backend would retrieve the user info in the app's DB

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,40 @@
 import { http, HttpResponse } from "msw";
+import type { ApplicationUser } from "../contexts/user-context";
+import { decodeJwt } from "./decode-jwt";
+
+function getUser(email: string): ApplicationUser | null {
+  return (
+    [
+      {
+        id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b303b3d",
+        name: "John Maverick",
+        firstName: "John",
+        lastName: "Maverick",
+        email: "john.maverick@example.com",
+      },
+      {
+        id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b31",
+        name: "Jane Doe",
+        firstName: "Jane",
+        lastName: "Doe",
+        email: "jane.doe@example.com",
+      },
+    ].find((user) => user.email === email) ?? null
+  );
+}
+
+const emailAttrNameInJwt = "email";
 
 export const handlers = [
   http.get("https://api.example.com/user", ({ request }) => {
     const token = request.headers.get("Authorization")?.split(" ")[1];
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "John",
-      lastName: "Maverick",
-      email: "john.maverick@example.com",
-      token,
-    });
+    const decoded = token ? decodeJwt(token) : null;
+    if (decoded) {
+      const user = getUser(decoded[emailAttrNameInJwt]);
+      return HttpResponse.json(user ?? { error: "Unauthorized" }, {
+        status: 401,
+      });
+    }
+    return HttpResponse.json({ error: "Unauthorized" }, { status: 401 });
   }),
 ];
